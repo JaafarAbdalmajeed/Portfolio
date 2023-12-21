415b1ae423ee504328a83d1782efad997c5aeadd
JaafarAlwahsh <jaafaralwahsh22@gmail.com>
2023-12-21 15:12:35 +0300

    file sql

---
+++ portfolio.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 21, 2023 at 01:11 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `portfolio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `about`
+--
+-- Error reading structure for table portfolio.about: #1932 - Table &#039;portfolio.about&#039; doesn&#039;t exist in engine
+-- Error reading data for table portfolio.about: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `portfolio`.`about`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `educations`
+--
+-- Error reading structure for table portfolio.educations: #1932 - Table &#039;portfolio.educations&#039; doesn&#039;t exist in engine
+-- Error reading data for table portfolio.educations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `portfolio`.`educations`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `professions`
+--
+-- Error reading structure for table portfolio.professions: #1932 - Table &#039;portfolio.professions&#039; doesn&#039;t exist in engine
+-- Error reading data for table portfolio.professions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `portfolio`.`professions`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projects`
+--
+-- Error reading structure for table portfolio.projects: #1932 - Table &#039;portfolio.projects&#039; doesn&#039;t exist in engine
+-- Error reading data for table portfolio.projects: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `portfolio`.`projects`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `skills`
+--
+-- Error reading structure for table portfolio.skills: #1932 - Table &#039;portfolio.skills&#039; doesn&#039;t exist in engine
+-- Error reading data for table portfolio.skills: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `portfolio`.`skills`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table portfolio.users: #1932 - Table &#039;portfolio.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table portfolio.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `portfolio`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
